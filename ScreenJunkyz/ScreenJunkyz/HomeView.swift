e13522490a0bec2cbe0f00a5b4bf2faa641e054d
Srinivas <Srinivas.rowdur@gmail.com>
2022-02-04 20:08:00 +0000

    Changed the loading screen to a custom screen

---
+++ ScreenJunkyz/ScreenJunkyz/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  ScreenJunkyz
+//
+//  Created by Srinivas Rowdur on 2/4/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Srini Hello!")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
