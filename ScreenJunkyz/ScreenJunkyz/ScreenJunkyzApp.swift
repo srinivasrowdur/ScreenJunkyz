6f749465c28d6deb64612e1d6ea7475906b9ddbe
Srinivas <Srinivas.rowdur@gmail.com>
2022-02-04 19:52:19 +0000

    Added a blank IOS App to the source code

---
+++ ScreenJunkyz/ScreenJunkyz/ScreenJunkyzApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenJunkyzApp.swift
+//  ScreenJunkyz
+//
+//  Created by Srinivas Rowdur on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ScreenJunkyzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScreenJunkyz/ScreenJunkyzUITests/ScreenJunkyzUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ScreenJunkyzUITestsLaunchTests.swift
+//  ScreenJunkyzUITests
+//
+//  Created by Srinivas Rowdur on 2/4/22.
+//
+
+import XCTest
+
+class ScreenJunkyzUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
